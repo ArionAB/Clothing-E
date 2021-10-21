9789d6bb44bc03d3f4f30bb4d900f57c04b4f265
Mihai <mihai.arion0@gmail.com>
2021-10-21 11:13:35 +0300

    implemented firebase utils, included ability to store autehnticated users into our firebase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { ShopPage } from "./pages/Shop/shop.component";
 import { Header } from "./components/header/header.component";
 import SigninAndSignUpPage from "./pages/Sign-in-up/sign-in-up";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -20,9 +20,30 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          console.log(snapShot); // has an ID
+          console.log(snapShot.data()); //has information about user but no ID
+
+          this.setState(
+            {
+              //we created this object so the current user can have the ID and the DATA
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state); //made a callback because this.setState is async so we have to wait
+              // for results otherwise it will be null
+            }
+          );
+        });
+      } else this.setState({ currentUser: userAuth }); //equivalent of saying null if
+      //  the user is not logged in
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+import { useRef } from "react";
 
 const config = {
   apiKey: "AIzaSyDG4r-48MIQxDJTtmJQvH5Up5aCtHthI0w",
@@ -10,6 +11,33 @@ const config = {
   messagingSenderId: "103153936396",
   appId: "1:103153936396:web:a762da9b4f696d5e536629",
   measurementId: "G-TDRQDYVY5Z",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  console.log(userRef);
+
+  const snapShot = await userRef.get();
+  console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
