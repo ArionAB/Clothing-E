dfdf63cbdbaa9aff017ce6d642f6aa83e8e6f66e
Mihai <mihai.arion0@gmail.com>
2021-12-22 14:32:25 +0200

    Added redux-persist for session storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { userReducer } from "./user/user.reducer";
 import { cartReducer } from "./Cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+  //we dont want to hold the session for user because firebase is already doing that, otherwise we would include it as well
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
